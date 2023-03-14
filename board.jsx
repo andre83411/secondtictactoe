8a50872761d67d5f4279620be806e934af6fdeb9
Andrea Almiron <andreamontes.rei@gmail.com>
2023-03-14 16:53:02 -0600

    updated game

--- board.jsx
+++ board.jsx
@@ -1,8 +1,8 @@
 const Square = ({id, newState}) => {
     const [status, setStatus] = React.useState(null);
-    const xo = ["O", "X", " "];
+    const xo = ["O", "X"];
     return (
-        <button onClick= {(e)=>{
+        <button onClick= {()=>{
             let nextPlayer = newState(id);
             setStatus(nextPlayer);  
         }}>
@@ -10,81 +10,106 @@ const Square = ({id, newState}) => {
         </button>
     );
 };
-    
+
 const Board = () => {
     const [player, setPlayer] = React.useState(1);
     const [state, setState] = React.useState(Array(9).fill(null));
-    //set state here
-    
-    let status = `Player ${player}`;
-    let winner = checkWinner(state);
-    if (winner != null) status = `Player ${winner} wins`;
-     
-    //function to keep track of total state
-    const newState = idOfSquare=> {
-       state[idOfSquare] = player; //player is present player
-       let thePlayer = player;
-       setState(state); // state is array of 0 or 1 or null
-       let nextplayer =(player + 1) % 2;
-
-       if(winner == null) {
-       setPlayer(nextplayer);
-       } else { setPlayer(xo[2])} //stops game
-
-       return thePlayer;
+  
+    // Function to update the state and switch players
+    const newState = (idOfSquare) => {
+      if (state[idOfSquare] !== null) {
+        // Square already clicked
+        return;
+      }
+  
+      // Update square state
+      state[idOfSquare] = player;
+      setState(state);
+  
+      // Check for a winner
+      const winner = checkWinner(state);
+      if (winner !== null) {
+        setPlayer(null); // Stop the game
+        return player;
+      }
+  
+      // Switch players
+      const nextplayer = (player + 1) % 2;
+      setPlayer(nextplayer);
+  
+      return player;
+    };
+  
+    // Determine the game status message
+    let status;
+    const winner = checkWinner(state);
+    if (winner !== null) {
+      status = `Player ${winner} wins!`;
+    } else if (player !== null) {
+      status = `Player ${player}'s turn`;
+    } else {
+      status = "Game over!";
     }
-
-    function renderSquare(i) {
-        return <Square id={i} newState={newState}></Square>;
-    }
-
+  
+    // Render the squares
+    const renderSquare = (i) => {
+      return <Square id={i} newState={newState} />;
+    };
+  
+    // Render the board
     return (
-        <div className="game-board">
-            <div className="grid-row">
-                {renderSquare(0)}
-                {renderSquare(1)}
-                {renderSquare(2)}
-            </div>
-            <div className="grid-row">
-                {renderSquare(3)}
-                {renderSquare(4)}
-                {renderSquare(5)}
-            </div>
-            <div className="grid-row">
-                {renderSquare(6)}
-                {renderSquare(7)}
-                {renderSquare(8)}
-            </div>
-            <div id="info">
-                <h1>{status}</h1>
-            </div>
+      <div className="game-board">
+        <div className="grid-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
         </div>
+        <div className="grid-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="grid-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+        <div id="info">
+          <h1>{status}</h1>
+        </div>
+      </div>
     );
-};
+  };
+  
+    
 
 function checkWinner(state) {
     const win = [
-        //horizontal winner
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        //vertical winner
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        //diagonal winner
-        [0, 4, 8],
-        [2, 4, 6]
+      //horizontal winner
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      //vertical winner
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      //diagonal winner
+      [0, 4, 8],
+      [2, 4, 6]
     ];
-
+  
     for (let i = 0; i < win.length; i++) {
-        const [a, b, c] = win[i];
-        if(state[a] == state[b] && state[a] == state[c] && state[a])
+      const [a, b, c] = win[i];
+      if (state[a] !== null && state[a] === state[b] && state[a] === state[c]) {
         return state[a];
-
-    };
+      }
+    }
     return null;
-}
+  };
+
+
 // ===========================================================
 
 ReactDOM.render(<Board/>, document.getElementById("root"));
+
+
